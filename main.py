45cdb471fb973f814bca0ea51ae5a1fcd4acc31e
Nathaniel C. Moratto <me@itznxthaniel.dev>
2026-09-03 20:46:28 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,133 @@
+# Nathaniel C. Moratto
+# Symbol Lookup
+
+symbols = {
+    "`": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "~": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "!": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "@": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "#": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "$": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "%": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "^": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "&": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "*": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "(": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    ")": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "-": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "_": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "=": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "+": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "[": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "]": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "{": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "}": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "|": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "\\": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    ";": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    ":": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "'": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "\"": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    ",": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    ".": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "<": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    ">": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "/": {
+        "givenName": "",
+        "aliasNames": []
+    },
+    "?": {
+        "givenName": "",
+        "aliasNames": []
+    }
+}
