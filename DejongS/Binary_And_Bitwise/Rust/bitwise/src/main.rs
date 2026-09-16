38718299d7e9dfc18819ab8e1713ffeff63da296
sdejong <ScottADeJong@gmail.com>
2026-09-16 10:05:53 -0400

    Moved my stuff into DeJongS instead of MorattoN

--- DejongS/Binary_And_Bitwise/Rust/bitwise/src/main.rs
+++ DejongS/Binary_And_Bitwise/Rust/bitwise/src/main.rs
@@ -5,6 +5,7 @@ enum Operation {
     And,
     Or,
     Xor,
+    Not,
     Lshift,
     Rshift,
 }
@@ -15,6 +16,7 @@ impl Display for Operation {
             Operation::And => write!(f, "and"),
             Operation::Or => write!(f, "or"),
             Operation::Xor => write!(f, "xor"),
+            Operation::Not => write!(f, "not"),
             Operation::Lshift => write!(f, "lshift"),
             Operation::Rshift => write!(f, "rshift"),
         }
@@ -24,7 +26,7 @@ impl Display for Operation {
 fn get_valid_isize(value_number: i8) -> io::Result<i8> {
     loop {
         let mut raw_value = String::new();
-        print!("Enter a number between -255 and 255 for value {value_number}: ");
+        print!("Enter a number between -127 and 127 for value {value_number}: ");
         io::stdout().flush()?;
         io::stdin().read_line(&mut raw_value)?;
         match raw_value.trim().parse::<i8>() {
@@ -46,6 +48,7 @@ fn get_valid_operation() -> io::Result<Operation> {
             "and" | "&" => return Ok(And),
             "or" | "|" => return Ok(Or),
             "xor" | "^" => return Ok(Xor),
+            "not" | "!" => return Ok(Not),
             "lshift" | "<<" => return Ok(Lshift),
             "rshift" | ">>" => return Ok(Rshift),
             _ => eprintln!("Please enter a valid operation!"),
@@ -60,6 +63,7 @@ fn do_operation(value1: i8, value2: i8, operation: Operation) {
         And => value1 & value2,
         Or => value1 | value2,
         Xor => value1 ^ value2,
+        Not => !value1,
         Lshift => value1 << value2,
         Rshift => value1 >> value2,
     };
@@ -67,7 +71,7 @@ fn do_operation(value1: i8, value2: i8, operation: Operation) {
     println!("***********************************");
     println!("{value1} in binary:\t\t{value1:08b}");
     println!("{value2} in binary:\t\t{value2:08b}");
-    println!("{value1} {operation} {value2}: \t\t{result:08b}");
+    println!("{value1} {operation} {value2}: \t\t{result:08b}\t\t{result}");
 }
 
 fn main() -> io::Result<()> {
